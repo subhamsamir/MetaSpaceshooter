c9263937b606561bbe2279ecaa65a0ed6bf1c748
Subham samir <ssrp2814@gmail.com>
2023-06-27 10:19:20 +0530

    section6

---
+++ Assets/SpaceShooter2022/Scripts/LaserGun.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//[RequireComponent(typeof(AudioSource))]
+public class LaserGun : MonoBehaviour
+{
+    [SerializeField] private Animator laserAnimator;
+    [SerializeField] private AudioClip laserSFX;
+    [SerializeField] private Transform raycastOrigin;
+
+    private AudioSource laserAudioSource;
+
+    private RaycastHit hit;
+
+    private void Awake()
+    {
+        laserAudioSource = GetComponent<AudioSource>();
+    }
+
+    public void LaserGunFired()
+    {
+        //animate the gun
+        laserAnimator.SetTrigger("Fire");
+
+        //play laser gun SFX
+        laserAudioSource.PlayOneShot(laserSFX);
+
+        //raycast
+        if (Physics.Raycast(raycastOrigin.position, raycastOrigin.forward, out hit, 800))
+        {
+            if (hit.transform.GetComponent<AsteroidHit>() != null)
+            {
+                hit.transform.GetComponent<AsteroidHit>().AsteroiDestroyed();
+            }
+        }
+    }
+}
